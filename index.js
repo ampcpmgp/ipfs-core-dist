5deebfd3815ed6bf36e5bed4bd0ecb4fffec7d20
ampcpmgp <ampcpmgp@gmail.com>
2021-04-18 10:42:24 +0900

    update module

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //
 // The NPM packages `ipfs` and `ipfs-core` both have type annotations
 // using docstrings, but because they're still not ESM capable
-// and only ships Node code by default Snowpack et al try to 
+// and only ships Node code by default Snowpack et al try to
 // import it into the browser which fails.
 //
 // The solution is to import the browser distribution from within
@@ -11,6 +11,6 @@
 // Thankfully TypeSciprt can do that. But it'd still be better if
 // we could cast an imported module directly in the import statement.
 // @ts-ignore
-import IPFS_ from 'ipfs-core/dist/index.min.js';
+import IPFS_ from "ipfs-core/dist/index.min.js";
 const IPFS = IPFS_;
 export default IPFS;
